72d389bfe71a7cd7050a52283b2ee8f8c8cc99df
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-21 20:53:44 -0500

    Moved get_walker to variable assignment. Index now returns formatted string.

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>' + get_walker() + ' should walk Gator next.</h1>'
+    walker = get_walker()
+    return '<h1>%s should walk Gator next</h1>' % walker
 
 if __name__ == '__main__':
     app.run(debug=True)
