9bdda773f6b7737619e3e512c3fa973883fd267f
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-24 13:28:22 -0500

    Changed get_walker to get_data. Now returns dict with walker and day/time of next walk.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-from spreadsheet import get_walker
+from spreadsheet import get_data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    walker = get_walker()
-    return render_template('index.html', name=walker)
+    data = get_data()
+    return render_template('index.html', name=data["walker"], day=data["day"], time=data["time"])
 
 if __name__ == '__main__':
     app.run(debug=True)
