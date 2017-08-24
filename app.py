2e13908cb51428a31e77336614cbe6b91b6d7805
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-24 18:54:14 -0500

    Added code to bind to port per heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from spreadsheet import get_data
 
@@ -10,4 +11,6 @@ def index():
     return render_template('index.html', name=data["walker"], day=data["day"], time=data["time"])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
